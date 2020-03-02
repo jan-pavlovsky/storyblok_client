c0ce723370cb7b58b378bb88396e450ee26357fb
Isak <16906089+IsakHansson@users.noreply.github.com>
2020-03-02 14:21:32 +0100

    Add README.md and code clean up

---
+++ example/README.md
@@ -0,0 +1,8 @@
+# storyblok_client
+
+Demonstrates how to use the storyblok_client plugin.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](http://flutter.io/).
--- lib/storyblok_client.dart
+++ lib/storyblok_client.dart
@@ -15,11 +15,6 @@ class ResolveRelations {
   ResolveRelations({this.componentName, this.fieldName});
 }
 
-enum Order {
-  ascending,
-  descending,
-}
-
 class SortBy {
   final String attributeField;
   final String contentField;
@@ -49,36 +44,20 @@ class FilterQuery {
 
   FilterQuery._({this.attribute, this.operation, this.value});
 
-  FilterQuery.contains({this.attribute, String value})
-      : operation = 'in',
-        value = value;
-  FilterQuery.notIn({this.attribute, String value})
-      : operation = 'not_in',
-        value = value;
+  FilterQuery.contains({this.attribute, String this.value}) : operation = 'in';
+  FilterQuery.notIn({this.attribute, String this.value}) : operation = 'not_in';
   FilterQuery.allInArray({this.attribute, List<String> value})
       : operation = 'all_in_array',
         value = value.reduce((previous, current) => ',$current');
   FilterQuery.inArray({this.attribute, List<String> value})
       : operation = 'in_array',
         value = value.reduce((previous, current) => ',$current');
-  FilterQuery.greaterThanDate({this.attribute, DateTime value})
-      : operation = 'gt-date',
-        value = value;
-  FilterQuery.lessThanDate({this.attribute, DateTime value})
-      : operation = 'lt-date',
-        value = value;
-  FilterQuery.greaterThanInt({this.attribute, int value})
-      : operation = 'gt-int',
-        value = value;
-  FilterQuery.lessThanInt({this.attribute, int value})
-      : operation = 'lt-int',
-        value = value;
-  FilterQuery.greaterThanFloat({this.attribute, double value})
-      : operation = 'gt-float',
-        value = value;
-  FilterQuery.lessThanFloat({this.attribute, double value})
-      : operation = 'lt-float',
-        value = value;
+  FilterQuery.greaterThanDate({this.attribute, DateTime this.value}) : operation = 'gt-date';
+  FilterQuery.lessThanDate({this.attribute, DateTime this.value}) : operation = 'lt-date';
+  FilterQuery.greaterThanInt({this.attribute, int this.value}) : operation = 'gt-int';
+  FilterQuery.lessThanInt({this.attribute, int this.value}) : operation = 'lt-int';
+  FilterQuery.greaterThanFloat({this.attribute, double this.value}) : operation = 'gt-float';
+  FilterQuery.lessThanFloat({this.attribute, double this.value}) : operation = 'lt-float';
 }
 
 class StoryblokClient {
@@ -99,31 +78,30 @@ class StoryblokClient {
     Map<String, String> parameters,
     bool ignoreCacheVersion = false,
   }) async {
-    if (parameters == null) parameters = {};
+    if (parameters == null) parameters = {'token': _token};
 
     if (!ignoreCacheVersion) {
       if (_autoCacheInvalidation) await invalidateCacheVersion();
 
-      if (_cacheVersion == null)
+      if (_cacheVersion == null) {
         print('No cache invalidation version fetched. Consider turning on auto cache invalidation');
-      else
+      } else {
         parameters['cv'] = _cacheVersion;
+      }
     }
 
-    parameters['token'] = _token;
-
-    print(Uri.https(_base, '/v1/cdn/$path', parameters).toString());
-
     final response = await http.get(Uri.https(_base, '/v1/cdn/$path', parameters));
-    if (response.statusCode != 200)
+    if (response.statusCode != 200) {
       throw ("Invalid response from Storyblok: ${response.statusCode}");
+    }
 
     return json.decode(response.body);
   }
 
   Future<void> invalidateCacheVersion() async {
-    if (_autoCacheInvalidation)
+    if (_autoCacheInvalidation) {
       print("Automatic cache invalidation is configured, avoid calling manually.");
+    }
 
     final data = await _get('spaces/me', ignoreCacheVersion: true);
     _cacheVersion = data['space']['version'].toString();
@@ -154,9 +132,10 @@ class StoryblokClient {
     if (uuid != null) parameters['find_by'] = 'uuid';
     if (version != null) parameters['version'] = EnumToString.parse(version);
     if (resolveLinks != null) parameters['resolve_links'] = resolveLinks.toString();
-    if (resolveRelations != null)
+    if (resolveRelations != null) {
       parameters['resolve_relations'] = resolveRelations.fold<String>(
           '', (previous, current) => previous += '${current.componentName}.${current.fieldName},');
+    }
     if (fromRelease != null) parameters['from_release'] = fromRelease;
     if (language != null) parameters['language'] = language;
     if (fallbackLanguage != null) parameters['fallback_language'] = fallbackLanguage;
@@ -185,23 +164,28 @@ class StoryblokClient {
   }) async {
     final parameters = <String, String>{};
     if (startsWith != null) parameters['starts_with'] = startsWith;
-    if (byUuids != null)
+    if (byUuids != null) {
       parameters['by_uuids'] = byUuids.reduce((previous, current) => previous += ',$current');
+    }
     if (fallbackLang != null) parameters['fallback_lang'] = fallbackLang;
-    if (byUuidsOrdered != null)
+    if (byUuidsOrdered != null) {
       parameters['by_uuids_ordered'] =
           byUuidsOrdered.reduce((previous, current) => previous += ',$current');
-    if (excludingIds != null)
+    }
+    if (excludingIds != null) {
       parameters['excluding_ids'] =
           excludingIds.reduce((previous, current) => previous += ',$current');
-    if (excludingFields != null)
+    }
+    if (excludingFields != null) {
       parameters['excluding_fields'] =
           excludingFields.reduce((previous, current) => previous += ',$current');
+    }
     if (version != null) parameters['version'] = EnumToString.parse(version);
     if (resolveLinks != null) parameters['resolve_links'] = resolveLinks.toString();
-    if (resolveRelations != null)
+    if (resolveRelations != null) {
       parameters['resolve_relations'] = resolveRelations.fold<String>(
           '', (previous, current) => previous += '${current.componentName}.${current.fieldName},');
+    }
     if (fromRelease != null) parameters['from_release'] = fromRelease;
     if (sortBy != null) {
       var sort =
@@ -212,10 +196,11 @@ class StoryblokClient {
       parameters['sort_by'] = sort;
     }
     if (searchTerm != null) parameters['search_term'] = searchTerm;
-    if (filterQueries != null)
+    if (filterQueries != null) {
       filterQueries.forEach((filter) =>
           parameters['filter_query[${filter.attribute}][${filter.operation}]'] =
               filter.value.toString());
+    }
     if (isStartpage != null) parameters['is_startpage'] = isStartpage ? '1' : '0';
     if (page != null) parameters['page'] = page.toString();
     if (perPage != null) parameters['per_page'] = perPage.toString();
